cc629b0c34251a3cd55628abc196c0c00571df8b
ziv <zivvv0@gmail.com>
2013-11-18 00:22:36 +0800

    save & get url

--- server/server/views.py
+++ server/server/views.py
@@ -4,13 +4,15 @@ from django.template import RequestContext
 
 import urllib2
 import re
+import os.path
 
 from tools import MultiPartForm
 
 url = 'http://www.outofservice.com/bigfive/'
 
 def test(request):
-  return render_to_response('test.html', context_instance=RequestContext(request));
+  return render_to_response('test.html',
+      context_instance=RequestContext(request));
 
 def post(request):
   if not request.POST:
@@ -26,13 +28,18 @@ def post(request):
   url_request.add_header('content-length', str(len(body)))
   url_request.add_data(body)
 
-  try:
-    url_response = urllib2.urlopen(url_request, timeout=20)
-    page = url_response.read()
-    iter = re.finditer(r'Your percentile: (\d*)<', page)
-    list = [i.group(1) for i in iter]
-    return render_to_response('result.html', {
-      'result' : list},
-      context_instance=RequestContext(request));
-  except Exception as e:
+  #try:
+  url_response = urllib2.urlopen(url_request, timeout=20)
+  get_url = url_response.geturl()
+  iter = re.finditer(r'R=([\d\.]*)&', get_url)
+  list = [i.group(1) for i in iter]
+  if not list:
     return HttpResponseRedirect('/')
+  fout = file(os.path.join(os.path.join(os.path.dirname(__file__), 'data'),
+    '1.txt'), 'w')
+  fout.write(str(list))
+  return render_to_response('result.html', {
+    'result' : list},
+    context_instance=RequestContext(request));
+  #except Exception as e:
+  #  return HttpResponseRedirect('/')
